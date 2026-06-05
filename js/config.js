a99771eaa8cd9c46d2e59318bb82118de1a82fc5
Francis Garapan <jhongarapan@gmail.com>
2026-06-05 21:23:00 +0800

    add supabase project keys

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-    url: "YOUR_SUPABASE_URL",
-    anonKey: "YOUR_SUPABASE_ANON_KEY"
+    url: "https://hlyfwtdyflhomvxbjzqz.supabase.co",
+    anonKey: "sb_publishable_4Cid9uvNgJ8u62IB4Uavlg_u4Cm2M1i"
 };
